7e24c3c5a8257e0d4751e31d04b64598025aa49c
Marrek Nožka <nozka@spseol.cz>
2024-05-31 15:26:22 +0200

    funguje

--- src/main.c
+++ src/main.c
@@ -1,43 +1,71 @@
 #include <stdbool.h>
 #include <stm8s.h>
-//#include <stdio.h>
 #include "main.h"
 #include "milis.h"
-//#include "delay.h"
-//#include "uart1.h"
+#include "uart1.h"
+#include "swi2c.h"
+#include <stdio.h>
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
-
     init_milis();
-    //init_uart1();
+    init_uart1();
+    swi2c_init();
 }
 
 
 int main(void)
 {
   
+
     uint32_t time = 0;
+    uint8_t precteno[10] = {0,0,0,0,0,0,0,0,0,0};
+    uint8_t zapsano[10] = {0,0,0,0,0,0,0,0,0,0};
+    uint8_t err;
+    uint16_t * p_teplota;
+    uint8_t teplota;
+    int8_t desetina;
+
 
     init();
 
-    while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
+    printf("\nScan I2C bus:\n");
+    printf("Recover: 0x%02X\n", swi2c_recover());
+    for (uint8_t addr = 0; addr < 128; addr++) {
+        if (swi2c_test_slave(addr<<1) == 0) {
+            printf("0x%02X \n", addr);
         }
-        //delay_ms(333);
+    }
+    printf("------------- scan end --------------------\n");
+
+    while (1) {
+        if (milis() - time > 1111 ) {
+            time = milis();
+
+            // čas
+            err = swi2c_read_buf(0x68, 0x00, precteno, 7);
+            printf("%d%d.%d%d. 20%d%d %d%d:%d%d:%d%d \n",       // v RTC obvodu je čas uložen v BCD
+                   precteno[4] >> 4, precteno[4] & 0x0F,
+                   precteno[5] >> 4, precteno[5] & 0x0F,
+                   precteno[6] >> 4, precteno[6] & 0x0F,
+                   precteno[2] >> 4, precteno[2] & 0x0F,
+                   precteno[1] >> 4, precteno[1] & 0x0F,
+                   precteno[0] >> 4, precteno[0] & 0x0F);
+
+            // teplota
+            err = swi2c_read_buf(0x48, 0x00, precteno, 2);
+            p_teplota = (uint16_t *) precteno;
+            teplota = precteno[0];
+            if (precteno[1] & 0b10000000) {
+                desetina = 5; 
+            } else {
+                desetina = 0; 
+            }
+            printf("%d,%d     <-   %02X\n", teplota, desetina, * p_teplota);
+
+        }
     }
 }
 
